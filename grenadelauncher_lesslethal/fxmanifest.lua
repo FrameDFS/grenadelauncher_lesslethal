1141e4d8a454701f903065e504281a393980e12f
FrameDFS <61120622+FrameDFS@users.noreply.github.com>
2020-10-26 12:46:56 +0100

    Update and rename __resource.lua to fxmanifest.lua

---
+++ grenadelauncher_lesslethal/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+game "gta5"
+
+name "grenadelauncher_lesslethal"
+description "Converted Grenade Launcher to Non-Letal Ammunition Launcher"
+author ": FrameDFS#1229"
+version "1.0"
+
+files {
+  'weapons.meta'
+}
+
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons.meta'
